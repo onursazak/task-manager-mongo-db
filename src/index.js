fb6bd0f28a9a737117c19756fb8b8d77a00cf771
Onur <onur.sazak@hotmail.com.tr>
2020-09-16 23:28:18 +0300

    Integrating async - await

--- src/index.js
+++ src/index.js
@@ -10,78 +10,134 @@ const port = process.env.PORT || 3000;
 // it automatically parses incoming JSON to an object.
 app.use(express.json())
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body);
 
-    user.save().then(() => {
+    // user.save().then(() => {
+    //     res.status(201).send(user);
+    // }).catch((error) => {
+    //     res.status(400).send(error);
+    // })
+
+    try {
+        await user.save();
         res.status(201).send(user);
-    }).catch((error) => {
+    } catch (error) {
         res.status(400).send(error);
-    })
+    }
+
 })
 
-app.get('/users', (req, res) => {
-    User.find({}).then((users) => {
+app.get('/users', async (req, res) => {
+
+    // User.find({}).then((users) => {
+    //     res.send(users);
+    // }).catch((e) => {
+    //     res.status(500).send();
+    // })
+
+    try {
+        const users = await User.find({});
         res.send(users);
-    }).catch((e) => {
+    } catch (error) {
         res.status(500).send();
-    })
+    }
+
 })
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
     const _id = req.params.id;
 
-    User.findById(_id).then((user) => {
+    // User.findById(_id).then((user) => {
+    //     if (!user) {
+    //         return res.status(404).send();
+    //     }
+    //     res.send(user);
+
+    // }).catch((e) => {
+    //     // if length of id is less than 12 , mongoose throw an cast error.Thus, it it will return 500 status code instead of 404.
+    //     // to prevent this we can use isValid check.
+    //     if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    //         return res.status(400).send({ error: 'Invalid ID!' })
+    //     }
+    //     res.status(500).send();
+    // })
+
+    const user = await User.findById(_id);
+    try {
         if (!user) {
             return res.status(404).send();
         }
         res.send(user);
-
-    }).catch((e) => {
-        // if length of id is less than 12 , mongoose throw an cast error.Thus, it it will return 500 status code instead of 404.
-        // to prevent this we can use isValid check.
-        if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-            return res.status(400).send({ error: 'Invalid ID!' })
-        }
+    } catch (error) {
         res.status(500).send();
-    })
-
+    }
 })
 
 
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
     const task = new Task(req.body);
-    task.save().then(() => {
-        console.log("task is : ");
-        console.log(task);
+
+    // task.save().then(() => {
+    //     console.log("task is : ");
+    //     console.log(task);
+    //     res.status(201).send(task);
+    // }).catch((error) => {
+    //     res.status(400).send(error);
+    // })
+
+    try {
+        const result = await task.save()
+        console.log("result is : --> " + result);
         res.status(201).send(task);
-    }).catch((error) => {
+    } catch (error) {
         res.status(400).send(error);
-    })
+    }
+
+
 })
 
-app.get('/tasks', (req, res) => {
-    Task.find({}).then((tasks) => {
-        res.send(tasks);
-    }).catch((e) => {
+app.get('/tasks', async (req, res) => {
+    // Task.find({}).then((tasks) => {
+    //     res.send(tasks);
+    // }).catch((e) => {
+    //     res.status(500).send();
+    // })
+
+    try {
+        res.send(await Task.find({}));
+    } catch (error) {
         res.status(500).send();
-    })
+    }
+
 })
 
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
     const _id = req.params.id;
-    
-    Task.findById(_id).then((task) => {
 
-        if (!task) {
-            return res.status(404).send();
-        }
+    // Task.findById(_id).then((task) => {
+
+    //     if (!task) {
+    //         return res.status(404).send();
+    //     }
+
+    //     res.send(task);
+
+    // }).catch((error) => {
+    //     res.status(500).send();
+    // })
+
+    try {
+        const task = await Task.findById({ _id });
+        
+        if(!task) return res.status(404).send();
 
         res.send(task);
 
-    }).catch((error) => {
+    } catch (error) {
         res.status(500).send();
-    })
+    }
+
 })
 
 app.listen(port, () => {
