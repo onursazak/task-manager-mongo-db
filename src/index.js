8dca8f8e399972e5147e8c5eed0549e1a0e23329
Onur <onur.sazak@hotmail.com.tr>
2020-09-19 13:54:54 +0300

    resource updating endpoints II

--- src/index.js
+++ src/index.js
@@ -164,6 +164,33 @@ app.get('/tasks/:id', async (req, res) => {
 
 })
 
+app.patch('/tasks/:id', async (req, res) => {
+
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['description', 'completed'];
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+    if(!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid request'});
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        
+        if(!task) {
+            res.status(404).send();
+        }
+
+        res.send(task);
+
+    }
+    catch (error) {
+        res.status(400).send(error);
+    }
+
+})
+
+
 app.listen(port, () => {
     console.log("server is up on port : " + port);
 })
