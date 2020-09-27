afbbf2a66c10676d6419eeb66b08f32271266611
Onur <onur.sazak@hotmail.com.tr>
2020-09-27 22:11:02 +0300

    logging in users

--- src/routers/user.js
+++ src/routers/user.js
@@ -12,7 +12,16 @@ router.post('/users', async (req, res) => {
     } catch (error) {
         res.status(400).send(error);
     }
-    
+
+});
+
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password);
+        res.send(user);
+    } catch(e) {
+        res.status(400).send();
+    }
 });
 
 router.get('/users', async (req, res) => {
@@ -67,19 +76,19 @@ router.patch('/users/:id', async (req, res) => {
     }
 });
 
-router.delete('/users/:id', async(req, res) => {
-    
+router.delete('/users/:id', async (req, res) => {
+
 
     try {
         const user = await User.findByIdAndDelete(req.params.id);
 
-        if(!user) {
+        if (!user) {
             return res.status(404).send();
         }
 
         res.send(user);
 
-    } catch(error) {
+    } catch (error) {
         res.status(500).send(error);
     }
 });
