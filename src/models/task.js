da84b5af3a00790461eae952d37ef101de3724d6
Onur <onur.sazak@hotmail.com.tr>
2020-11-01 15:02:26 +0200

    working with timestamps

--- src/models/task.js
+++ src/models/task.js
@@ -1,21 +1,25 @@
 const mongoose = require('mongoose');
 
-const taskSchema = new mongoose.Schema({
-    description: {
-        type: String,
-        trim: true,
-        required: true
+const taskSchema = new mongoose.Schema(
+    {
+        description: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        completed: {
+            type: Boolean,
+            default: false
+        },
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User' // to create a relationship between User and Task models.
+        }
     },
-    completed: {
-        type: Boolean,
-        default: false
-    },
-    owner: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'User' // to create a relationship between User and Task models.
-    }
-});
+    {
+        timestamps: true
+    });
 
 
 const Task = mongoose.model('Task', taskSchema);
