839f5e8cf617cfa7d9f8e316da683e2b07a107f6
Onur <onur.sazak@hotmail.com.tr>
2020-11-02 22:54:16 +0200

    sorting data

--- src/routers/task.js
+++ src/routers/task.js
@@ -22,13 +22,19 @@ router.post('/tasks', auth, async (req, res) => {
 })
 
 // GET /tasks?completed=true
-// GET /tasks?limit=10&skip=0
+// GET /tasks?limit=10&skip=20
+// GET /tasks?sortBy=createdAt:desc
 router.get('/tasks', auth, async (req, res) => {
 
     const match = {};
+    const sort = {};
 
     if(req.query.completed) {
         match.completed = req.query.completed === 'true';
+    }
+    if(req.query.sortBy) {
+        const parts = req.query.sortBy.split(':');
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
     }
 
     try {
@@ -44,7 +50,12 @@ router.get('/tasks', auth, async (req, res) => {
             match,
             options: {
                 limit: parseInt(req.query.limit), //limits the data that will be fetched.
-                skip: parseInt(req.query.skip)
+                skip: parseInt(req.query.skip),
+                sort
+                // sort: {
+                //     //createdAt: -1, // 1 for ascending(newest record first) , -1 for descending (oldest record first),
+                //     completed: 1 // 1 for completed = false values first , -1 for completed = true values first. 
+                // }
             }
         }).execPopulate();
 
