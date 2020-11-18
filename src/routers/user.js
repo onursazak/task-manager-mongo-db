131870e9bdaee44e0a9d323133887ef2053b1ce7
Onur <onur.sazak@hotmail.com.tr>
2020-11-19 00:57:42 +0200

    adding and deleting and images - user profile

--- src/routers/user.js
+++ src/routers/user.js
@@ -81,8 +81,6 @@ router.patch('/users/me', auth, async (req, res) => {
 });
 
 const upload = multer({
-    //destination to save the file.
-    dest: 'avatars',
     limits: {
         fileSize: 1000000
     },
@@ -96,10 +94,18 @@ const upload = multer({
         
 })
 
-router.post('/users/me/avatar', upload.single('avatar') , (req,res) => {
+router.post('/users/me/avatar', auth, upload.single('avatar') , async (req,res) => {
+    req.user.avatar = req.file.buffer;
+    await req.user.save();
     res.send();
 }, (error, req, res, next) => {
     res.status(400).send({error: error.message});
+})
+
+router.delete('/users/me/avatar', auth, async (req,res) => {
+    req.user.avatar = undefined;
+    await req.user.save();
+    res.send();
 })
 
 router.delete('/users/me', auth, async (req, res) => {
